1e2c74a3622feadf73baee03a202b1be76817ef4
gucioopielka <gucioopielka@gmail.com>
2024-10-30 11:42:54 +0100

    globals path correction

--- utils/globals.py
+++ utils/globals.py
@@ -6,7 +6,7 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 globals_path = os.path.join(base_dir, '..', 'globals.yml')
 
 # Load data from YAML
-with open("globals.yml", "r") as stream:
+with open(globals_path, "r") as stream:
     data = yaml.safe_load(stream)
 
 # Define global variables
